5d5177334e4d9d153eba3e66006842159367aa10
Roberto Robson <betorobson@gmail.com>
2017-05-08 11:21:20 -0300

    Rr/f/b7 (#8)
    * linha 1\nlinha2
    
    * linha 1
    linha 2
    linha 3
    
    * first module
    
    * first module ext

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-7
+
+var Module = (function(){
+
+	var test = true;
+
+	var module = {
+	   getTest: function(){
+	   	return test;
+	   }
+	};
+
+	return module;
+})();
+
+var ModuleExt = (function(mod){
+
+	var mod = mod || {};
+
+	var ext = function(){
+		return mod.getTest().length + ' ext';
+	};
+
+	mod.ext = ext;
+
+	return mod;
+
+})(Module);
+
