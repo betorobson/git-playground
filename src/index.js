ac1eb0a2e07f5471ae9b3786159b74f2787c0bb5
Roberto Robson <betorobson@gmail.com>
2017-05-08 11:17:17 -0300

    first module ext

--- src/index.js
+++ src/index.js
@@ -12,3 +12,17 @@ var Module = (function(){
 	return module;
 })();
 
+var ModuleExt = (function(mod){
+
+	var mod = mod || {};
+
+	var ext = function(){
+		return mod.getTest().length + ' ext';
+	};
+
+	mod.ext = ext;
+
+	return mod;
+
+})(Module);
+
