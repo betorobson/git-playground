6753bfc9c0f4ba28a1e3d0b0c34ed8c2b2665ae5
Roberto Robson <betorobson@gmail.com>
2017-10-16 21:09:01 -0200

    test git HEAD detached

--- src/home/home.js
+++ src/home/home.js
@@ -1,4 +1,5 @@
 
-console.log('home.js');
-console.log('commit 2');
-console.log('commit 3');
+		console.log('1. home.js');
+		console.log('2. commit 2');
+		console.log('3. commit 3');
+		console.log('4. commit 4');
