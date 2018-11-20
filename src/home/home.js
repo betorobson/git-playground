d65f8c0b0ef9663f6632705d9b7d233fb52e9f19
Roberto Robson <betorobson@gmail.com>
2018-11-20 10:49:55 -0200

    rr/feature2 (#38)
    * remove logs 1
    
    * rmeove logs 2

--- src/home/home.js
+++ src/home/home.js
@@ -1,7 +1,2 @@
 
 		console.log('1. home.js');
-		console.log('2. commit 2');
-		console.log('3. commit 3');
-		console.log('4. commit 4');
-		console.log('5. commit 5');
-		console.log('6. commit 6');
